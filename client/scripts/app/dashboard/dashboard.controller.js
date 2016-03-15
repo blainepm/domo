9e42279e4e4ab8d277e4a9b6ea34d61f39743a49
Romain <rbeaulieu@adista.fr>
2016-03-15 18:01:59 +0100

    add widget

--- client/scripts/app/dashboard/dashboard.controller.js
+++ client/scripts/app/dashboard/dashboard.controller.js
@@ -1,13 +1,22 @@
 'use strict';
 
 angular.module('blaineApp')
-    .controller('DashboardController', function ($scope, Principal, Weather) {
-        Principal.identity().then(function(account) {
-            $scope.account = account;
-            $scope.isAuthenticated = Principal.isAuthenticated;
-        });
-
-        Weather.getForcast(function(forcast) {
-            console.log(forcast);
-        });
+.controller('DashboardController', function ($scope, $filter, Principal, Weather) {
+    Principal.identity().then(function(account) {
+        $scope.account = account;
+        $scope.isAuthenticated = Principal.isAuthenticated;
     });
+
+    $scope.forcast = null;
+    $scope.weather = null;
+
+    Weather.getForcast(function(forcast) {
+        $scope.forcast = forcast;
+    });
+
+    Weather.getWeather(function(weather){
+        $scope.weather = weather;
+
+        weather.main.tempdegre = $filter('number')( (weather.main.temp - 273.15), 1);
+    });
+});
